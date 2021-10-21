4a0f075484b00ff7d39c5aa14d208af3ccd3504d
Yong Lin <yonglin@me.com>
2021-10-22 01:10:32 +0800

    Added display for picking skills and items

--- src/lib/Invention.svelte
+++ src/lib/Invention.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
     import { Universe } from '$lib/EveData';
 
+    let types = [];
+
     let scienceSkills = [];
+    let encryptionSkills = [];
+
+    $: {
+        types = Object.keys($Universe.types).map(type_id=>$Universe.types[type_id]);
+    }
+
     $: {
         scienceSkills = $Universe.markets.groups[375].types.map(type_id=>$Universe.types[type_id]).sort((a,b)=>a.name.localeCompare(b.name));
+        encryptionSkills = scienceSkills.filter(type=>type.name.indexOf("Encryption") >=0);
     }
 </script>
+
+<p>
+    <label>
+        Item to invent
+        <select>
+            {#each types as {id,name} }
+                <option value={id}>{name}</option>
+            {/each}
+        </select>
+    </label>
+</p>
 
 
 <label>
@@ -18,11 +38,19 @@
 </label>
 <br />
 <label>
-    Science skill 2
+    <select>
+        {#each scienceSkills as {id,name} }
+            <option value={id}>{name}</option>
+        {/each}
+    </select>
     <input type="range" min={0} max={5} />
 </label>
 <br />
 <label>
-    Racial encryption skill
+    <select>
+        {#each encryptionSkills as {id,name} }
+            <option value={id}>{name}</option>
+        {/each}
+    </select>
     <input type="range" min={0} max={5} />
 </label>
