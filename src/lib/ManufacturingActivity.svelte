a6b9e6013dbf3ff0a883e4c4b06e02fa75092bb7
Yong Lin <yonglin@me.com>
2021-10-24 02:23:28 +0800

    Added product buy/sell orders

--- src/lib/ManufacturingActivity.svelte
+++ src/lib/ManufacturingActivity.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Universe, Industry, MANUFACTURING_ACTIVITY_ID  } from "$lib/EveData";
+    import { Universe, Industry, MANUFACTURING_ACTIVITY_ID, Type_Id  } from "$lib/EveData";
     import type { IndustryType, IndustryActivity, EntityCollection } from "$lib/EveData";
     import { getMarketType, MarketType } from "$lib/EveMarkets";
 
@@ -7,8 +7,15 @@
 
     export let blueprint: IndustryType = null;
 
-    let materialStores = [];
+    let relatedTypeStores = [];
     let relatedTypes: EntityCollection<MarketType> = {};
+
+    function subscribeToTypeStore( type_id: Type_Id ) {
+        relatedTypeStores.push( getMarketType(type_id).subscribe(value=>{
+            relatedTypes[type_id] = value;
+        }) );
+    }
+
 
     let manufacturing: IndustryActivity = null;
     $: {
@@ -16,17 +23,19 @@
 
         if(nextManufacturing != manufacturing) {
             // Unsubscribe from current material stores
-            materialStores.forEach(unsubscribe=>unsubscribe());
+            relatedTypeStores.forEach(unsubscribe=>unsubscribe());
 
             manufacturing = nextManufacturing;
-            materialStores = [];
+            relatedTypeStores = [];
             relatedTypes = {};
 
             // Request for new materials
             for(let type_id in manufacturing.materials) {
-                materialStores.push( getMarketType(parseInt(type_id)).subscribe(value=>{
-                    relatedTypes[type_id] = value;
-                }) );
+                subscribeToTypeStore( parseInt(type_id) );
+            }
+            // And the manufactured product(s)
+            for(let type_id in manufacturing.products) {
+                subscribeToTypeStore( parseInt(type_id) );
             }
         }
     }
@@ -40,12 +49,24 @@ No blueprint selected yet
     <dd>{manufacturing.time}</dd>
 </dl>
 
+Products
+<dl>
+    {#each Object.keys(manufacturing.products) as type_id}
+        <dt>{$Universe.types[type_id].name} [{type_id}]</dt>
+        <dd>
+            {manufacturing.products[type_id].quantity}x
+            Highest buy: {relatedTypes[type_id].orders.buy[0] && relatedTypes[type_id].orders.buy[0].price}
+            Lowest sell: {relatedTypes[type_id].orders.sell[0] && relatedTypes[type_id].orders.sell[0].price}
+        </dd>
+    {/each}
+</dl>
+
 Materials
 <dl>
     {#each Object.keys(manufacturing.materials) as type_id}
         <dt>{$Universe.types[type_id].name} [{type_id}]</dt>
         <dd>
-            {manufacturing.materials[type_id].quantity} 
+            {manufacturing.materials[type_id].quantity}x
             Highest buy: {relatedTypes[type_id].orders.buy[0] && relatedTypes[type_id].orders.buy[0].price}
             Lowest sell: {relatedTypes[type_id].orders.sell[0] && relatedTypes[type_id].orders.sell[0].price}
         </dd>
