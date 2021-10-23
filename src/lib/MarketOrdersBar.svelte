cc85ccea3247ea19b1fbd41694576f4380418e53
Yong Lin <yonglin@me.com>
2021-10-24 03:58:47 +0800

    Added catch if extents are 0

--- src/lib/MarketOrdersBar.svelte
+++ src/lib/MarketOrdersBar.svelte
@@ -21,16 +21,23 @@
     function scale(value: number): number {
         return 0 + width*(value-extents[0])/(extents[1]-extents[0]);
     }
+
+    let validExtents = false;
+    $: {
+        validExtents = extents[1] > extents[0];
+    }
 </script>
 
 <svg style="background: #ccc" {width} {height} viewBox={`0 0 ${300} ${height}`} xmlns="http://www.w3.org/2000/svg">
-    {#if highestBuyOrder && highestBuyOrder.price !== null}
-        <rect x={scale(highestBuyOrder.price*quantity)} fill="red" stroke="none" width={1} height={height} />
-    {/if}
-    {#if lowestSellOrder && lowestSellOrder.price !== null}
-        <rect x={scale(lowestSellOrder.price*quantity)} fill="green" stroke="none" width={1} height={height} />
-    {/if}
-    {#if totalCost !== null}
-        <rect x={scale(totalCost)} fill="#333" stroke="none" width={1} height={height} />
+    {#if validExtents}
+        {#if highestBuyOrder && highestBuyOrder.price !== null}
+            <rect x={scale(highestBuyOrder.price*quantity)} fill="red" stroke="none" width={1} height={height} />
+        {/if}
+        {#if lowestSellOrder && lowestSellOrder.price !== null}
+            <rect x={scale(lowestSellOrder.price*quantity)} fill="green" stroke="none" width={1} height={height} />
+        {/if}
+        {#if totalCost !== null}
+            <rect x={scale(totalCost)} fill="#333" stroke="none" width={1} height={height} />
+        {/if}
     {/if}
 </svg>
