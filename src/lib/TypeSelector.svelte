b565e9b087901403432b6913798dc7b909969f4e
Yong Lin <yonglin@me.com>
2021-11-09 16:27:10 +0800

    Migrated type breakdown to use MarketGroupSelector

--- src/lib/TypeSelector.svelte
+++ src/lib/TypeSelector.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
-    import { Universe } from '$lib/eve-data/EveData';
+    import type { EveTypeId } from '$lib/eve-data/EveTypes';
+    import EveTypes from '$lib/eve-data/EveTypes';
 
-    import type { MarketGroup_Id, Type_Id, MarketGroup, Type, EntityCollection } from '$lib/eve-data/EveData';
-import { createEventDispatcher } from 'svelte';
-    
-    let selectableMarketGroupIds: Set<MarketGroup_Id> = new Set();
+    import { createEventDispatcher } from 'svelte';
 
-    let groupTreeBranches:Array<EntityCollection<MarketGroup>> = [];
-    let selectedGroupTreeBranches = [null];
-
-    let types: Array<Type> = [];
-    let filteredTypes: Array<Type> = [];
-
-    export let selectableTypes: Array<Type> = undefined;
+    export let selectableTypes: Array<EveTypeId> = [];
     export let selectedTypeId:number = null;
 
     const dispatch = createEventDispatcher();
@@ -20,106 +12,14 @@ import { createEventDispatcher } from 'svelte';
         dispatch('change', parseInt(event.target.value));
     }
 
-    $: {
-        if($Universe.types) {
-            types = selectableTypes || Object.values($Universe.types);
-        }
-    }
-
-    function getChildGroupIds(group:MarketGroup) : Array<MarketGroup_Id> {
-        if(group.child_groups === undefined) return [group.market_group_id];
-
-        let childGroups = Object.keys(group.child_groups).map(id=>parseInt(id));
-
-        for(let childGroupId in group.child_groups) {
-            childGroups = childGroups.concat( getChildGroupIds(group.child_groups[childGroupId]) )
-        }
-
-        return childGroups;
-    }
-
-    $: {
-        let smallestSelectedBranch = selectedGroupTreeBranches[selectedGroupTreeBranches.length-1] || selectedGroupTreeBranches[selectedGroupTreeBranches.length-2];
-        
-        if(smallestSelectedBranch) {
-            // Find all sub types under the branch
-            filteredTypes = [];
-            let filteredTypeIDs: Set<Type_Id> = new Set();
-            let selectableTypeIDs: Set<Type_Id> = new Set(types.map(type=>type.type_id));
-
-            let group_ids = getChildGroupIds( $Universe.markets.groups[smallestSelectedBranch] );
-
-            for(let market_group_id of group_ids) {
-                $Universe.markets.groups[market_group_id].types.forEach( type_id=>{
-                    if(selectableTypeIDs.has(type_id)) 
-                        filteredTypeIDs.add(type_id)
-                } );
-            }
-
-            filteredTypes = Array.from(filteredTypeIDs).map(id=>$Universe.types[id]);
-        } else {
-            filteredTypes = types;
-        }
-
-        // Return first 1000 results for performance reasons
-        filteredTypes.sort((a,b)=>a.name.localeCompare(b.name));   
-    }
-
-    $: {
-        if($Universe.markets && $Universe.types && $Universe.markets.groups) {
-            let selectableTypeIDs: Set<Type_Id> = new Set(types.map(type=>type.type_id));
-
-            selectableMarketGroupIds.clear();
-            for(let group_id in $Universe.markets.groups) {
-                let group = $Universe.markets.groups[group_id];
-
-                for(let type_id of group.types) {
-                    if(selectableTypeIDs.has(type_id)) {
-                        let g = parseInt(group_id);
-                        do {
-                            selectableMarketGroupIds.add(g);
-                            g = $Universe.markets.groups[g].parent_group_id;
-                        } while(g !== undefined)
-                    }
-                }
-            }
-            selectableMarketGroupIds = selectableMarketGroupIds;
-        }
-    }
-
-    $: {
-        if($Universe.markets)
-        {
-            groupTreeBranches = [$Universe.markets.groupTree];
-            for(let selectedBranch of selectedGroupTreeBranches) {
-                if(selectedBranch != null && $Universe.markets.groups[selectedBranch].child_groups)
-                    groupTreeBranches.push($Universe.markets.groups[selectedBranch].child_groups)
-            }
-        }
-    }
 </script>
 
-<p>
-    {#each groupTreeBranches as marketGroup, i}
-        <select name={i.toString()} bind:value={selectedGroupTreeBranches[i]} on:change={(event)=>{
-            let selectedMarketGroup = marketGroup[event.target.value];
-            if(selectedMarketGroup.child_groups) {
-                selectedGroupTreeBranches = [...selectedGroupTreeBranches.slice(0, i+1), null];
-            }
-        }}>
-            {#each Object.values(marketGroup).filter(group=>selectableMarketGroupIds.has(group.market_group_id)) as {market_group_id, name} }
-                <option value={market_group_id}>{name}</option>
-            {/each}
-        </select>
+Item 
+<select value={selectedTypeId} on:change={onSelectionChange}>
+    {#each selectableTypes as typeId }
+        <option value={typeId}>{$EveTypes.get(typeId)?.name ?? typeId}</option>
     {/each}
-    <br />
-    Item 
-    <select value={selectedTypeId} on:change={onSelectionChange}>
-        {#each filteredTypes as {type_id,name} }
-            <option value={type_id}>{name}</option>
-        {/each}
-    </select>
-</p>
+</select>
 
 <style lang="scss">
     select {
--- src/routes/breakdown/[type].svelte
+++ src/routes/breakdown/[type].svelte
@@ -11,42 +11,44 @@
     import TypeSelector from "$lib/TypeSelector.svelte";
     import ManufacturingActivity from "$lib/ManufacturingActivity.svelte";
     import ReactionActivity from "$lib/ReactionActivity.svelte";
+    import { DescendantGroups, EveMarketGroupId, GetProducibleTypes } from "$lib/eve-data/EveMarketGroups";
+    import MarketGroupSelector from "$lib/MarketGroupSelector.svelte";
+    import EveTypes, { EveType, EveTypeId, MarketGroupToTypes } from "$lib/eve-data/EveTypes";
     
-
     $: selectedProductId = parseInt( $page.params['type'] ) || null;
 
+    let selectedGroup: EveMarketGroupId;
+    let _prevSelectedProductId;
+    $: {
+        // Only update selectedGroup if there is a change,
+        // so the user can select the group freely without being affected
+        if(selectedProductId !== _prevSelectedProductId) {
+            // Only change the group if the type is not producible in the current group,
+            // so that the list of types doesn't change 
+            if(!GetProducibleTypes(selectedGroup, $DescendantGroups, $MarketGroupToTypes, $ProductToActivity).includes(selectedProductId)) {
+                selectedGroup = $EveTypes.get(selectedProductId)?.market_group_id;
+            }
+        }
+        _prevSelectedProductId = selectedProductId;
+    }
 
     // Only pick types that can be manufactured
-    let selectableTypes: Array<Type> = null;
-
-    $: if($Industry.types && $Universe.types) {
-        function getManufacturedProducts(blueprint_id:Type_Id|string): Array<Type_Id> {
-            return Object.keys( $Industry.types[blueprint_id].activities[REACTION_ACTIVITY_ID]?.products ?? $Industry.types[blueprint_id].activities[MANUFACTURING_ACTIVITY_ID]?.products ?? {} )
-                .map(id=>parseInt(id));
-        }
-
-        let selectableTypeIDs = [];
-
-        Object.values($Industry.types).forEach(t=>{
-            selectableTypeIDs = [...selectableTypeIDs, ...getManufacturedProducts(t.type_id)]
-        })
-
-        selectableTypes = selectableTypeIDs.filter(id=>$Universe.types[id]!==undefined).map(id=>$Universe.types[id]);
-    }
+    $: selectableTypes = GetProducibleTypes(selectedGroup, $DescendantGroups, $MarketGroupToTypes, $ProductToActivity);
 
     $: selectedActivityId = $ProductToActivity.get(selectedProductId)?.activity.activity.activityID;
 </script>
 
 
 <svelte:head>
-	<title>{($Universe?.types && $Universe?.types[selectedProductId]) ? `${$Universe?.types[selectedProductId]?.name} - ` : "" }EVE Online Visual Industry Calculator</title>
+	<title>{$EveTypes.get(selectedProductId) ? `${$EveTypes.get(selectedProductId).name} - ` : "" }EVE Online Visual Industry Calculator</title>
 </svelte:head>
 
+<p>
+<MarketGroupSelector value={selectedGroup} on:change={event=>{selectedGroup = event.detail}} /><br/>
+<TypeSelector selectedTypeId={selectedProductId} {selectableTypes}
+    on:change={event=>{goto(`${event.detail}`, {keepfocus:true})}} />
+</p>
 
-<TypeSelector selectedTypeId={selectedProductId} on:change={event=>{goto(`${event.detail}`, {keepfocus:true})}} {selectableTypes} />
-
-
-<p/>
 
 {#if selectedActivityId === REACTION_ACTIVITY_ID}
 <ReactionActivity productTypeId={selectedProductId} />
