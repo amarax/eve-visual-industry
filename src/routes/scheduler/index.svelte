9d1cbe8e837ab5a2efc68f75f3cb3f726a967c49
Yong Lin <yonglin@me.com>
2021-11-12 11:58:11 +0800

    Added simple inventory input

--- src/routes/scheduler/index.svelte
+++ src/routes/scheduler/index.svelte
@@ -35,6 +35,9 @@ import EveTypes from "$lib/eve-data/EveTypes";
         materialsList = materialsList;
     }
 
+    let materialsInventory: {[index: EveTypeId]: Quantity} = {}
+
+
     function timeout(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
@@ -43,7 +46,9 @@ import EveTypes from "$lib/eve-data/EveTypes";
     async function copyBOMToClipboard() {
         let bom = "";
         for(let [materialTypeId, quantity] of materialsList.entries()) {
-            bom += `${$EveTypes.get(materialTypeId).name} ${quantity}\n`;
+            let requiredQuantity = quantity - (materialsInventory[materialTypeId]??0)
+            if(requiredQuantity > 0)
+                bom += `${$EveTypes.get(materialTypeId).name} ${quantity}\n`;
         }
         await navigator.clipboard.writeText(bom);
         copied = true;
@@ -65,6 +70,29 @@ import EveTypes from "$lib/eve-data/EveTypes";
 
     button.fixedWidth {
         width: 150px;
+    }
+
+
+    .itemName {
+        display: inline-block;
+        width: 200px;
+        overflow-x: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+    }
+
+    .qty {
+        display: inline-block;
+		text-align: right;
+		margin-right: 10px;
+        width: 70px;
+	}
+
+    .inventory {
+        input[type=number] {
+            width: 70px;
+            text-align: right;
+        }
     }
 </style>
 
@@ -89,7 +117,8 @@ import EveTypes from "$lib/eve-data/EveTypes";
 <b>Bill of Materials</b> <button class="fixedWidth" on:click={copyBOMToClipboard} disabled={copied}>{copied?"Copied!":"Copy to clipboard"}</button><br/>
 {#each [...materialsList.entries()] as [materialTypeId, quantity]}
     <span class="itemName">{$EveTypes.get(materialTypeId).name}</span>
-    <span class="qty">{quantity}</span>
+    <span class="qty">{Math.max(quantity - (materialsInventory[materialTypeId]??0), 0)}</span>
+    <span class="inventory"><input type="number" bind:value={materialsInventory[materialTypeId]} /></span>
     <br/>
 {/each}
 </p>
